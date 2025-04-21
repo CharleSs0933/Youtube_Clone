470dbe0c2a75a3bd8f42553afd6b21fe7b8c7b88
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-04-22 03:30:38 +0700

    Setup tRPC

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div className=""></div>;
+import { HydrateClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  const data = await trpc.hello({ text: "CharleSs" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading....</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}></ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
