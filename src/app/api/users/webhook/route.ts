7a65012b8895545f9179acb9abebd461b2806474
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-04-22 02:01:46 +0700

    Update Webhook

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -1,4 +1,7 @@
+import { db } from "@/db";
+import { users } from "@/db/schema";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { eq } from "drizzle-orm";
 import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   try {
@@ -6,12 +9,37 @@ export async function POST(req: NextRequest) {
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data;
     const eventType = evt.type;
-    console.log(
-      `Received webhook with ID ${id} and event type of ${eventType}`
-    );
-    console.log("Webhook payload:", evt.data);
+
+    if (eventType === "user.created") {
+      await db.insert(users).values({
+        clerkId: evt.data.id,
+        name: `${evt.data.first_name} ${evt.data.last_name}`,
+        imageUrl: evt.data.image_url,
+      });
+    }
+
+    if (eventType === "user.deleted") {
+      if (!evt.data.id) {
+        return new Response("No user id provided", { status: 400 });
+      }
+
+      await db.delete(users).where(eq(users.clerkId, evt.data.id));
+    }
+
+    if (eventType === "user.updated") {
+      if (!evt.data.id) {
+        return new Response("No user id provided", { status: 400 });
+      }
+
+      await db
+        .update(users)
+        .set({
+          name: `${evt.data.first_name} ${evt.data.last_name}`,
+          imageUrl: evt.data.image_url,
+        })
+        .where(eq(users.clerkId, evt.data.id));
+    }
 
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
