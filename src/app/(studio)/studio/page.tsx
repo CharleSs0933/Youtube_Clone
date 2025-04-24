fa1521f8d4464c77700b31be12c7f1101fb05019
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-04-24 17:38:38 +0700

    Studio Videos

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { DEFAULT_LIMIT } from "@/constants";
+import StudioView from "@/modules/studio/ui/views/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
 
-const Page = () => {
-  return <div>Studio</div>;
+export const dynamic = "force-dynamic";
+
+const Page = async () => {
+  void trpc.studio.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 };
 
 export default Page;
