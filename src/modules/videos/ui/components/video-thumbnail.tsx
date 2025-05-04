a8a6b46ca692a80e4123c929d5f40f88dc825dfe
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-05-04 18:37:56 +0700

    Update preview Url for video

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 interface VideoThumbnailProps {
-  imageUrl?: string;
+  imageUrl?: string | null;
 }
 
 const VideoThumbnail = ({ imageUrl }: VideoThumbnailProps) => {
