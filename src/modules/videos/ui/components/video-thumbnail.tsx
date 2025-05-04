ea13734a179f2abeb53c21863e1a8ae0f37d3de4
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-05-04 18:49:26 +0700

    Update duration for video

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -2,18 +2,30 @@ import Image from "next/image";
 
 interface VideoThumbnailProps {
   imageUrl?: string | null;
+  previewUrl?: string | null;
+  title: string;
 }
 
-const VideoThumbnail = ({ imageUrl }: VideoThumbnailProps) => {
+const VideoThumbnail = ({
+  imageUrl,
+  title,
+  previewUrl,
+}: VideoThumbnailProps) => {
   return (
-    <div className="relative">
+    <div className="relative group">
       {/* Thumbnail wrapper */}
       <div className="relative w-full overflow-hidden rounded-xl aspect-video">
         <Image
           src={imageUrl ?? "/placeholder.svg"}
-          alt="Thumbnail"
+          alt={title}
           fill
-          className="size-full object-cover"
+          className="size-full object-cover group-hover:opacity-0"
+        />
+        <Image
+          src={previewUrl ?? "/placeholder.svg"}
+          alt={title}
+          fill
+          className="size-full object-cover opacity-0 group-hover:opacity-100"
         />
       </div>
 
